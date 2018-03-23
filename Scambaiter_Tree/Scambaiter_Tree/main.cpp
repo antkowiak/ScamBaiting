e1e2edb3bab9c869899e7d30b35bb0cc772f2f66
Ryan Antkowiak <antkowiak@gmail.com>
2018-03-23 06:51:14 -0500

    Add files via upload

---
+++ Scambaiter_Tree/Scambaiter_Tree/main.cpp
@@ -0,0 +1,54 @@
+#include <cstdlib>
+#include <iostream>
+#include "windows.h"
+
+int main(int argc, char *argv[])
+{
+	DWORD delayMs = 300;
+
+	std::cout << "Running Tree Scan Version 10.3.51 ..." << std::endl;
+	Sleep(delayMs);
+	std::cout << std::endl;
+	Sleep(delayMs);
+	Sleep(delayMs);
+	std::cout << "    oxoxoo    ooxoo" << std::endl;
+	Sleep(delayMs);
+	std::cout << "  ooxoxo oo  oxoxooo" << std::endl;
+	Sleep(delayMs);
+	std::cout << " oooo xxoxoo ooo ooox" << std::endl;
+	Sleep(delayMs);
+	std::cout << " oxo o oxoxo  xoxxoxo" << std::endl;
+	Sleep(delayMs);
+	std::cout << "  oxo xooxoooo o ooo" << std::endl;
+	Sleep(delayMs);
+	std::cout << "    ooo\oo\  /o/o" << std::endl;
+	Sleep(delayMs);
+	std::cout << "        \  \/ /" << std::endl;
+	Sleep(delayMs);
+	std::cout << "         |   /" << std::endl;
+	Sleep(delayMs);
+	std::cout << "         |  |" << std::endl;
+	Sleep(delayMs);
+	std::cout << "         | D|" << std::endl;
+	Sleep(delayMs);
+	std::cout << "         |  |" << std::endl;
+	Sleep(delayMs);
+	std::cout << "         |  |" << std::endl;
+	Sleep(delayMs);
+	std::cout << "  ______/____\____" << std::endl;
+	Sleep(delayMs);
+	std::cout << std::endl;
+	Sleep(delayMs);
+	Sleep(delayMs);
+	std::cout << "Tree Virus Scan Completed.   Viruses found: 0" << std::endl;
+	std::cout << "                             Trojans found: 0" << std::endl;
+	std::cout << "                               Worms found: 0" << std::endl;
+	std::cout << "                            Problems found: 0" << std::endl;
+	std::cout << std::endl;
+
+	Sleep(delayMs);
+	Sleep(delayMs);
+	Sleep(delayMs);
+
+	return EXIT_SUCCESS;
+}
